963c1f23bd84a695e706745429506e01d46fe90e
Raul Silva <raul.moonrocklabs@gmail.com>
2025-08-29 15:04:57 -0300

    refactor(ui): remove heading and update color scheme
    - Remove unnecessary heading from main page
    - Update global CSS to use light theme by default
    - Adjust fluid.js parameters for lighter background and reduced splat effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main>
       <FluidBackground />
-      <h1 style={{ position: "relative", zIndex: 1 }}>
-        Minha Página com Background Fluido 🌊
-      </h1>
     </main>
   );
 }
